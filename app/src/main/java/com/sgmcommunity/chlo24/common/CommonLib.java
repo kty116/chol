9bb7aa27deeeb35c7860f3d0e4869ae35ab1bd29
kyoungae <kyoungae3017@gmail.com>
2017-06-30 14:02:58 +0900

    CommonLib Class에 gps 상태 체크 메소드 추가

--- app/src/main/java/com/sgmcommunity/chlo24/common/CommonLib.java
+++ app/src/main/java/com/sgmcommunity/chlo24/common/CommonLib.java
@@ -2,7 +2,7 @@ package com.sgmcommunity.chlo24.common;
 
 import android.content.Context;
 import android.os.Build;
-import android.util.Log;
+import android.provider.Settings;
 import android.webkit.CookieManager;
 import android.webkit.CookieSyncManager;
 
@@ -15,14 +15,17 @@ import cz.msebera.android.httpclient.client.protocol.ClientContext;
 import cz.msebera.android.httpclient.cookie.Cookie;
 import cz.msebera.android.httpclient.protocol.HttpContext;
 
-import static com.google.android.gms.internal.zzs.TAG;
-
 /**
  * Created by hunter on 2017-05-29.
  */
 
 public class CommonLib{
 
+    /**
+     * 쿠키 보내는 메소드
+     * @param mhttpClient
+     * @param mContext
+     */
     public static void cookieMaker(AsyncHttpClient mhttpClient, Context mContext) {
         //롤리팝 이하 버전 cookiesyncmanager로 사용
 
@@ -48,6 +51,17 @@ public class CommonLib{
         }else {
             CookieManager.getInstance().flush();
         }
+    }
+
+    /**
+     * gps 상태 체크
+     * @param context
+     * @return
+     */
+    public static boolean checkLocationServicesStatus(Context context) {
+        String gpsEnabled = android.provider.Settings.Secure.getString(context.getContentResolver(), Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
+
+        return gpsEnabled.matches(".*gps.*");
     }
 
 }
